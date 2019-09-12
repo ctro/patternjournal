0aa1fd68c307a8b81bc7f6705ffdca411198beee
ctro <clint@ctro.net>
2019-09-11 20:24:21 -0600

    more async fixes in tests

--- test/routes/dates.test.js
+++ test/routes/dates.test.js
@@ -7,6 +7,9 @@ var db = require("../../models");
 describe("/ the root path", () => {
   test("Can get a protected path", async () => {
     await request(app).get("/days")
-      .then(response => expect(response.statusCode).toBe(200))
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      })
   });
+
 });
--- test/routes/index.test.js
+++ test/routes/index.test.js
@@ -6,37 +6,43 @@ var db = require("../../models");
 
 describe("/ the root path", () => {
   test("GET / with lots of sanity checks", async () => {
-    const response = await request(app).get("/");
-
-    expect(response.statusCode).toBe(200);
-    expect(response.charset).toBe("utf-8");
-    expect(response.type).toBe("text/html");
-    expect(response.clientError).toBe(false);
-    expect(response.serverError).toBe(false);
-    expect(response.error).toBe(false);
-
-    expect(response.text).toMatch(/Pattern Journal/);
+    await request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.charset).toBe("utf-8");
+        expect(response.type).toBe("text/html");
+        expect(response.clientError).toBe(false);
+        expect(response.serverError).toBe(false);
+        expect(response.error).toBe(false);
+        expect(response.text).toMatch(/Pattern Journal/);
+      });
   });
 });
 
 describe("Google OAuth Login", () => {
   //our Auth happens via Google
   test("GET /login redirects to Google", async () => {
-    await request(app).get("/login")
+    await request(app)
+      .get("/login")
       .then(response => {
         expect(response.statusCode).toBe(302); //redirect
-        expect(response.headers['location']).toMatch(/https:\/\/accounts.google.com/);
-      })
+        expect(response.headers["location"]).toMatch(
+          /https:\/\/accounts.google.com/
+        );
+      });
   });
 
   // this is the OAuth callback, which does the same auth
   // handled behind the scenes by Passport
   test("GET /loggedin redirects to Google", async () => {
-    await request(app).get("/loggedin")
+    await request(app)
+      .get("/loggedin")
       .then(response => {
         expect(response.statusCode).toBe(302); //redirect
-        expect(response.headers['location']).toMatch(/https:\/\/accounts.google.com/);
-      })
+        expect(response.headers["location"]).toMatch(
+          /https:\/\/accounts.google.com/
+        );
+      });
   });
-
 });
